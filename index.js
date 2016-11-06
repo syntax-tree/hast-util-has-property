dc366b0690b0378b2141bad161cf9906d982dcec
Titus Wormer <tituswormer@gmail.com>
2016-11-06 14:47:07 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,49 +1,22 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module hast:util:has-property
- * @fileoverview Check whether a node has a property.
- */
-
 'use strict';
 
-/* eslint-env commonjs */
+/* Dependencies. */
+var has = require('has');
 
-/*
- * Methods.
- */
-
-var has = Object.prototype.hasOwnProperty;
-
-/**
- * Check if `node` has `name`.
- *
- * @param {Node?} node - Node to check.
- * @param {string} name - Property to check.
- * @return {boolean} - Whether `node` has `name`.
- */
-function hasProperty(node, name) {
-    var props;
-    var value;
-
-    if (
-        !node ||
-        !name ||
-        typeof node !== 'object' ||
-        node.type !== 'element'
-    ) {
-        return false;
-    }
-
-    props = node.properties;
-    value = props && has.call(props, name) && node.properties[name];
-
-    return value !== null && value !== undefined && value !== false;
-}
-
-/*
- * Expose.
- */
-
+/* Expose. */
 module.exports = hasProperty;
+
+/* Check if `node` has a set `name` property. */
+function hasProperty(node, name) {
+  var props;
+  var value;
+
+  if (!node || !name || typeof node !== 'object' || node.type !== 'element') {
+    return false;
+  }
+
+  props = node.properties;
+  value = props && has(props, name) && props[name];
+
+  return value !== null && value !== undefined && value !== false;
+}
--- test.js
+++ test.js
@@ -1,91 +1,61 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module hast:util:has-property
- * @fileoverview Test suite for `has-property`.
- */
-
 'use strict';
 
-/* eslint-env node */
-
-/*
- * Module dependencies.
- */
-
+/* Dependencies. */
 var test = require('tape');
 var hasProperty = require('./index.js');
 
-/*
- * Tests.
- */
-
+/* Tests. */
 test('hasProperty', function (t) {
-    t.equal(
-        hasProperty(null, 'alpha'),
-        false,
-        'should return `false` without node'
-    );
+  t.equal(
+    hasProperty(null, 'alpha'),
+    false,
+    'should return `false` without node'
+  );
 
-    t.equal(
-        hasProperty({
-            'type': 'text',
-            'value': 'alpha'
-        }, 'bravo'),
-        false,
-        'should return `false` without `element`'
-    );
+  t.equal(
+    hasProperty({type: 'text', value: 'alpha'}, 'bravo'),
+    false,
+    'should return `false` without `element`'
+  );
 
-    t.equal(
-        hasProperty({
-            'type': 'element'
-        }, 'charlie'),
-        false,
-        'should return `false` without properties'
-    );
+  t.equal(
+    hasProperty({type: 'element'}, 'charlie'),
+    false,
+    'should return `false` without properties'
+  );
 
-    t.equal(
-        hasProperty({
-            'type': 'element',
-            'properties': {}
-        }, 'toString'),
-        false,
-        'should return `false` for prototypal properties'
-    );
+  t.equal(
+    hasProperty({type: 'element', properties: {}}, 'toString'),
+    false,
+    'should return `false` for prototypal properties'
+  );
 
-    t.equal(
-        hasProperty({
-            'type': 'element',
-            'properties': {
-                'id': 'delta'
-            }
-        }, 'echo'),
-        false,
-        'should return `false` if the property does not exist'
-    );
+  t.equal(
+    hasProperty({
+      type: 'element',
+      properties: {id: 'delta'}
+    }, 'echo'),
+    false,
+    'should return `false` if the property does not exist'
+  );
 
-    t.equal(
-        hasProperty({
-            'type': 'element',
-            'properties': {
-                'id': 'delta'
-            }
-        }),
-        false,
-        'should return `false` if without `name`'
-    );
+  t.equal(
+    hasProperty({
+      type: 'element',
+      properties: {id: 'delta'}
+    }),
+    false,
+    'should return `false` if without `name`'
+  );
 
-    t.equal(
-        hasProperty({
-            'type': 'element',
-            'properties': {
-                'id': 'delta'
-            }
-        }, 'id'),
-        true,
-        'should return `true` if the property does exist'
-    );
+  t.equal(
+    hasProperty({
+      type: 'element',
+      properties: {id: 'delta'}
+    }, 'id'),
+    true,
+    'should return `true` if the property does exist'
+  );
 
-    t.end();
+  t.end();
 });
