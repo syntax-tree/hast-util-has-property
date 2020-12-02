f3842625bd6eca7502f6e8564ec6b71f850c49b4
Titus Wormer <tituswormer@gmail.com>
2020-12-02 11:23:05 +0100

    Refactor to improve bundle size

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ module.exports = hasProperty
 
 // Check if `node` has a set `name` property.
 function hasProperty(node, name) {
-  var props
-  var value
-
-  if (!node || !name || typeof node !== 'object' || node.type !== 'element') {
-    return false
-  }
-
-  props = node.properties
-  value = props && own.call(props, name) && props[name]
+  var value =
+    node &&
+    node.type === 'element' &&
+    node.properties &&
+    own.call(node.properties, name) &&
+    node.properties[name]
 
   return value !== null && value !== undefined && value !== false
 }
