8aabe35a4ca7712927fd3783f435591a6d2290fe
Titus Wormer <tituswormer@gmail.com>
2023-08-01 10:32:28 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -1,67 +1,73 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {hasProperty} from './index.js'
-import * as mod from './index.js'
 
-test('hasProperty', () => {
-  assert.deepEqual(
-    Object.keys(mod).sort(),
-    ['hasProperty'],
-    'should expose the public api'
+test('hasProperty', async function (t) {
+  await t.test('should expose the public api', async function () {
+    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+      'hasProperty'
+    ])
+  })
+
+  await t.test('should return `false` without node', async function () {
+    assert.equal(hasProperty(null, 'alpha'), false)
+  })
+
+  await t.test('should return `false` without `element`', async function () {
+    assert.equal(hasProperty({type: 'text', value: 'alpha'}, 'bravo'), false)
+  })
+
+  await t.test('should return `false` without properties', async function () {
+    assert.equal(hasProperty({type: 'element'}, 'charlie'), false)
+  })
+
+  await t.test(
+    'should return `false` for prototypal properties',
+    async function () {
+      assert.equal(
+        hasProperty({type: 'element', properties: {}}, 'toString'),
+        false
+      )
+    }
   )
 
-  assert.equal(
-    hasProperty(null, 'alpha'),
-    false,
-    'should return `false` without node'
+  await t.test(
+    'should return `false` if the property does not exist',
+    async function () {
+      assert.equal(
+        hasProperty(
+          {
+            type: 'element',
+            properties: {id: 'delta'}
+          },
+          'echo'
+        ),
+        false
+      )
+    }
   )
 
-  assert.equal(
-    hasProperty({type: 'text', value: 'alpha'}, 'bravo'),
-    false,
-    'should return `false` without `element`'
-  )
+  await t.test('should return `false` if without `name`', async function () {
+    assert.equal(
+      // @ts-expect-error: check how a missing name is handled.
+      hasProperty({type: 'element', properties: {id: 'delta'}}),
+      false
+    )
+  })
 
-  assert.equal(
-    hasProperty({type: 'element'}, 'charlie'),
-    false,
-    'should return `false` without properties'
-  )
-
-  assert.equal(
-    hasProperty({type: 'element', properties: {}}, 'toString'),
-    false,
-    'should return `false` for prototypal properties'
-  )
-
-  assert.equal(
-    hasProperty(
-      {
-        type: 'element',
-        properties: {id: 'delta'}
-      },
-      'echo'
-    ),
-    false,
-    'should return `false` if the property does not exist'
-  )
-
-  assert.equal(
-    // @ts-expect-error runtime.
-    hasProperty({type: 'element', properties: {id: 'delta'}}),
-    false,
-    'should return `false` if without `name`'
-  )
-
-  assert.equal(
-    hasProperty(
-      {
-        type: 'element',
-        properties: {id: 'delta'}
-      },
-      'id'
-    ),
-    true,
-    'should return `true` if the property does exist'
+  await t.test(
+    'should return `true` if the property does exist',
+    async function () {
+      assert.equal(
+        hasProperty(
+          {
+            type: 'element',
+            properties: {id: 'delta'}
+          },
+          'id'
+        ),
+        true
+      )
+    }
   )
 })
